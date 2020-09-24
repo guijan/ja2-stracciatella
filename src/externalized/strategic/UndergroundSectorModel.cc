a057bb85923f8a72bd42349b249e5451e28ae89c
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2020-09-24 21:57:48 +0800

    Disable checks that are not relevant in UB campaign

--- src/externalized/strategic/UndergroundSectorModel.cc
+++ src/externalized/strategic/UndergroundSectorModel.cc
@@ -96,7 +96,8 @@ void UndergroundSectorModel::validateData(const std::vector<const UndergroundSec
 		if (iter == ugSectors.end())
 		{
 			ST::string err = ST::format("An underground sector is expected at ({}), but is not defined.", basement.AsLongString());
-			throw std::runtime_error(err.to_std_string());
+			//throw std::runtime_error(err.to_std_string());
+			SLOGW(err);
 		}
 	}
 }
